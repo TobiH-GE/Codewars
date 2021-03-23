83a90bd8e6fbca599aebb06ea00b45d4cdbebfd1
tobias.hartwich <tobias.hartwich@hotmail.de>
2021-03-23 17:34:51 +0100

    upload empty solution

---
+++ Next_bigger_number_with_the_same_digits/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Next_bigger_number_with_the_same_digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // https://www.codewars.com/kata/55983863da40caa2c900004e solution by TobiH
+        }
+    }
+    public class Kata
+    {
+        public static long NextBiggerNumber(long n)
+        {
+            //code me
+            return 0;
+        }
+    }
+}
